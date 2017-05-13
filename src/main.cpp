2498784650d5602f19ec7de84d4bab9e96d1d96d
dawidd6 <dawidd0811@gmail.com>
2017-05-13 15:06:40 +0200

    10th source drop

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,10 @@
 #include <QMessageBox>
 
 /* TESTING CLASSESS */
-class Game;
+class Board
+{
+
+};
 
 /* FUNCTIONS */
 void markDisabledAll(QPushButton button[3][3])
@@ -19,6 +22,21 @@ void markDisabledAll(QPushButton button[3][3])
 		button[x][y].setDisabled(true);
 }
 
+void drawLineOnGrid(QGridLayout &grid, QLabel &line, int fromrow, int fromcolumn, int rowspan, int columnspan, QPushButton button[3][3])
+{
+	grid.addWidget(&line, fromrow, fromcolumn, rowspan, columnspan, Qt::AlignJustify);
+	line.show();
+	markDisabledAll(button);
+}
+
+void markButtonIcon(QPushButton button[3][3], const int &x, const int &y, char button_str[3][3], const char symbol, QIcon &icon, QSize &size, bool &xnow, bool n, QLabel &label_current, QSize &size_current)
+{
+	button[x][y].setIcon(icon);
+	button[x][y].setIconSize(size);
+	button_str[x][y] = symbol;
+	label_current.setPixmap(icon.pixmap(size_current));
+	xnow = n;
+}
 
 void paintCross(QIcon &icon, QSize &size, const int &thickness)
 {
@@ -87,8 +105,8 @@ int main(int argc, char *argv[])
 
 	/* GRID_GAME */
 	QWidget widget_game;
-	QGridLayout grid_game;
-	
+QGridLayout grid_game;
+
 	/* BOX_MENU */
 	QWidget widget_menu;
 	QVBoxLayout box_menu;
@@ -121,7 +139,7 @@ int main(int argc, char *argv[])
 	QLabel horizon_line;
 	QLabel vertical_line;
 	QLabel turn("Whose turn:");
-	QLabel icon_current;
+	QLabel label_current;
 
 	/* PAINT STUFF */
 	paintCross(icon_x, size_x, thickness);
@@ -132,7 +150,7 @@ int main(int argc, char *argv[])
 	paintLine(right_line, -45, 420, QPointF(0, 100), thickness);
 
 	/* INITS */
-	icon_current.setPixmap(icon_x.pixmap(size_current));
+	label_current.setPixmap(icon_x.pixmap(size_current));
 	restart.setFocusPolicy(Qt::NoFocus);
 	play_2v2.setFixedSize(305, 40);
 	play_single.setFixedSize(305, 40);
@@ -165,6 +183,7 @@ int main(int argc, char *argv[])
 	line[1].setLineWidth(2);
 	line[2].setLineWidth(2);
 	line[3].setLineWidth(2);
+
 	for(int x = 0; x < 3; x++) for(int y = 0; y < 3; y++)
 	{
 		button[x][y].setFixedSize(100, 100);
@@ -183,7 +202,7 @@ int main(int argc, char *argv[])
 	{
 		xnow = true;
 		win = 'n';
-		icon_current.setPixmap(icon_x.pixmap(size_current));
+		label_current.setPixmap(icon_x.pixmap(size_current));
 		for(int x = 0; x < 3; x++) for(int y = 0; y < 3; y++)
 		{
 			button_str[x][y] = '0';
@@ -231,122 +250,45 @@ int main(int argc, char *argv[])
 	{
 		QObject::connect(&button[x][y], &QPushButton::clicked, [&, x, y]
 		{
-			if(xnow)
-			{
-				button[x][y].setIcon(icon_x);
-				button[x][y].setIconSize(size_x);
-				button_str[x][y] = 'x';
-				icon_current.setPixmap(icon_o.pixmap(size_current));
-				xnow = false;
-			}
-			else
-			{
-				button[x][y].setIcon(icon_o);
-				button[x][y].setIconSize(size_o);
-				button_str[x][y] = 'o';
-				icon_current.setPixmap(icon_x.pixmap(size_current));
-				xnow = true;
-			}
 			button[x][y].setDisabled(true);
 
-			//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+			if(xnow)
+				markButtonIcon(button, x, y, button_str, 'x', icon_x, size_x, xnow, false, label_current, size_current);
+			else
+				markButtonIcon(button, x, y, button_str, 'o', icon_o, size_o, xnow, true, label_current, size_current);
+
 			if(button_str[0][0] == 'x' && button_str[0][1] == 'x' && button_str[0][2] == 'x')
-			{
-				grid_game.addWidget(&horizon_line, 0, 0, 1, 5, Qt::AlignJustify);
-				horizon_line.show();
-				markDisabledAll(button);
-			}
+				drawLineOnGrid(grid_game, horizon_line, 0, 0, 1, 5, button);
 			else if(button_str[1][0] == 'x' && button_str[1][1] == 'x' && button_str[1][2] == 'x')
-			{
-				grid_game.addWidget(&horizon_line, 2, 0, 1, 5, Qt::AlignJustify);
-				horizon_line.show();
-				markDisabledAll(button);
-			}
+				drawLineOnGrid(grid_game, horizon_line, 2, 0, 1, 5, button);
 			else if(button_str[2][0] == 'x' && button_str[2][1] == 'x' && button_str[2][2] == 'x')
-			{
-				grid_game.addWidget(&horizon_line, 4, 0, 1, 5, Qt::AlignJustify);
-				horizon_line.show();
-				markDisabledAll(button);
-			}
-			/*else if(button_str[0] == 'x' && button_str[3] == 'x' && button_str[6] == 'x')
-			{
-				grid_game.addWidget(&vertical_line, 0, 0, 5, 1, Qt::AlignJustify);
-				vertical_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[1] == 'x' && button_str[4] == 'x' && button_str[7] == 'x')
-			{
-				grid_game.addWidget(&vertical_line, 0, 2, 5, 1, Qt::AlignJustify);
-				vertical_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[2] == 'x' && button_str[5] == 'x' && button_str[8] == 'x')
-			{
-				grid_game.addWidget(&vertical_line, 0, 4, 5, 1, Qt::AlignJustify);
-				vertical_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[0] == 'x' && button_str[4] == 'x' && button_str[8] == 'x')
-			{
-				grid_game.addWidget(&right_line, 0, 0, 5, 5, Qt::AlignJustify);
-				right_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[2] == 'x' && button_str[4] == 'x' && button_str[6] == 'x')
-			{
-				grid_game.addWidget(&left_line, 0, 0, 5, 5, Qt::AlignJustify);
-				left_line.show();
-				markDisabledAll();
-			}
-			// OOOOOOOOOOOOOOOOOOOOOOOOOOO
-			if(button_str[0] == 'o' && button_str[1] == 'o' && button_str[2] == 'o')
-			{
-				grid_game.addWidget(&horizon_line, 0, 0, 1, 5, Qt::AlignJustify);
-				horizon_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[3] == 'o' && button_str[4] == 'o' && button_str[5] == 'o')
-			{
-				grid_game.addWidget(&horizon_line, 2, 0, 1, 5, Qt::AlignJustify);
-				horizon_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[6] == 'o' && button_str[7] == 'o' && button_str[8] == 'o')
-			{
-				grid_game.addWidget(&horizon_line, 4, 0, 1, 5, Qt::AlignJustify);
-				horizon_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[0] == 'o' && button_str[3] == 'o' && button_str[6] == 'o')
-			{
-				grid_game.addWidget(&vertical_line, 0, 0, 5, 1, Qt::AlignJustify);
-				vertical_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[1] == 'o' && button_str[4] == 'o' && button_str[7] == 'o')
-			{
-				grid_game.addWidget(&vertical_line, 0, 2, 5, 1, Qt::AlignJustify);
-				vertical_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[2] == 'o' && button_str[5] == 'o' && button_str[8] == 'o')
-			{
-				grid_game.addWidget(&vertical_line, 0, 4, 5, 1, Qt::AlignJustify);
-				vertical_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[0] == 'o' && button_str[4] == 'o' && button_str[8] == 'o')
-			{
-				grid_game.addWidget(&right_line, 0, 0, 5, 5, Qt::AlignJustify);
-				right_line.show();
-				markDisabledAll();
-			}
-			else if(button_str[2] == 'o' && button_str[4] == 'o' && button_str[6] == 'o')
-			{
-				grid_game.addWidget(&left_line, 0, 0, 5, 5, Qt::AlignJustify);
-				left_line.show();
-				markDisabledAll();
-			}*/
+				drawLineOnGrid(grid_game, horizon_line, 4, 0, 1, 5, button);
+			else if(button_str[0][0] == 'x' && button_str[1][0] == 'x' && button_str[2][0] == 'x')
+				drawLineOnGrid(grid_game, vertical_line, 0, 0, 5, 1, button);
+			else if(button_str[0][1] == 'x' && button_str[1][1] == 'x' && button_str[2][1] == 'x')
+				drawLineOnGrid(grid_game, vertical_line, 0, 2, 5, 1, button);
+			else if(button_str[0][2] == 'x' && button_str[1][2] == 'x' && button_str[2][2] == 'x')
+				drawLineOnGrid(grid_game, vertical_line, 0, 4, 5, 1, button);
+			else if(button_str[0][0] == 'x' && button_str[1][1] == 'x' && button_str[2][2] == 'x')
+				drawLineOnGrid(grid_game, right_line, 0, 0, 5, 5, button);
+			else if(button_str[0][2] == 'x' && button_str[1][1] == 'x' && button_str[2][0] == 'x')
+				drawLineOnGrid(grid_game, left_line, 0, 0, 5, 5, button);
+			if(button_str[0][0] == 'o' && button_str[0][1] == 'o' && button_str[0][2] == 'o')
+				drawLineOnGrid(grid_game, horizon_line, 0, 0, 1, 5, button);
+			else if(button_str[1][0] == 'o' && button_str[1][1] == 'o' && button_str[1][2] == 'o')
+				drawLineOnGrid(grid_game, horizon_line, 2, 0, 1, 5, button);
+			else if(button_str[2][0] == 'o' && button_str[2][1] == 'o' && button_str[2][2] == 'o')
+				drawLineOnGrid(grid_game, horizon_line, 4, 0, 1, 5, button);
+			else if(button_str[0][0] == 'o' && button_str[1][0] == 'o' && button_str[2][0] == 'o')
+				drawLineOnGrid(grid_game, vertical_line, 0, 0, 5, 1, button);
+			else if(button_str[0][1] == 'o' && button_str[1][1] == 'o' && button_str[2][1] == 'o')
+				drawLineOnGrid(grid_game, vertical_line, 0, 2, 5, 1, button);
+			else if(button_str[0][2] == 'o' && button_str[1][2] == 'o' && button_str[2][2] == 'o')
+				drawLineOnGrid(grid_game, vertical_line, 0, 4, 5, 1, button);
+			else if(button_str[0][0] == 'o' && button_str[1][1] == 'o' && button_str[2][2] == 'o')
+				drawLineOnGrid(grid_game, right_line, 0, 0, 5, 5, button);
+			else if(button_str[0][2] == 'o' && button_str[1][1] == 'o' && button_str[2][0] == 'o')
+				drawLineOnGrid(grid_game, left_line, 0, 0, 5, 5, button);
 
 		});
 	}
@@ -356,7 +298,7 @@ int main(int argc, char *argv[])
 	box_menu.addWidget(&play_2v2);
 	box_menu.addWidget(&play_multi);
 	grid_game.addWidget(&turn, 6, 0);
-	grid_game.addWidget(&icon_current, 6, 2);
+	grid_game.addWidget(&label_current, 6, 2);
 	grid_game.addWidget(&restart, 6, 4, Qt::AlignRight);
 	grid_game.addWidget(&return_to_menu, 7, 4, Qt::AlignRight);
 	for(int x = 0; x < 3; x++) for(int y = 0; y < 3; y++)
