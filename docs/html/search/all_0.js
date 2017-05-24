6292f1a824f5619859481ac41ec9e2f326f7c7f6
dawidd6 <dawidd0811@gmail.com>
2017-05-24 13:24:07 +0200

    initial doxygen

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['board2v2',['Board2v2',['../class_board2v2.html',1,'']]],
+  ['boardmulti',['BoardMulti',['../class_board_multi.html',1,'']]]
+];
---
+++ docs/html/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['qtictactoe_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fdawidd6_2fqtictactoe_2esvg_3fbranch_3ddevel_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fdawidd6_2fqtictactoe_29',['QTicTacToe [![Build Status](https://travis-ci.org/dawidd6/qtictactoe.svg?branch=devel)](https://travis-ci.org/dawidd6/qtictactoe)',['../md_README.html',1,'']]]
+];
