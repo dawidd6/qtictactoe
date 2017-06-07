dbb64273188d7acdc7eb30b94742bc2344a4da24
dawidd6 <dawidd0811@gmail.com>
2017-06-07 14:00:15 +0200

    deal with segmentation fault at exit

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
  */
 
 #include <QApplication>
-#include <QCoreApplication>
 #include <QtWidgets>
 #include <QtNetwork>
 
@@ -18,41 +17,23 @@
 
 int main(int argc, char *argv[])
 {
-	QCoreApplication *a = nullptr;
-	Game *game = nullptr;
-	Server *server = nullptr;
-
-	try
+	if(argc < 2)
 	{
-		if(argc < 2)
-		{
-			a = new QApplication(argc, argv);
-			game = new Game;
-		}
-		else if(QString(argv[1]) == "server")
-		{
-			a = new QCoreApplication(argc, argv);
-			server = new Server;
-		}
-		else
-		{
-			qDebug("Wrong argument\n\n"
-					"\tqtictactoe server - run headless server\n"
-					"\tqtictactoe - run graphical client\n");
-			return 1;
-		}
+		QApplication a(argc, argv);
+		Game game;
+		return a.exec();
 	}
-	catch(std::bad_alloc &ba)
+	else if(QString(argv[1]) == "server")
 	{
-		qDebug("Exception caught: %s", ba.what());
-		abort();
+		QCoreApplication a(argc, argv);
+		Server server;
+		return a.exec();
 	}
-
-	QObject::connect(a, &QCoreApplication::aboutToQuit, [&game, &server]
+	else
 	{
-		if(game != nullptr) delete game;
-		if(server != nullptr) delete server;
-	});
-
-	return a->exec();
+		qDebug("Wrong argument\n\n"
+				"\tqtictactoe server - run headless server\n"
+				"\tqtictactoe - run graphical client\n");
+		return 1;
+	}
 }
